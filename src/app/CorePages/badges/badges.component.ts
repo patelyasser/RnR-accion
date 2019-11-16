8f30f501a0b9cb1f32dcd8d8d3264dda6b0873fd
Yasser Patel <yasser.patel@accionlabs.com>
2019-11-16 12:26:48 +0530

    Update badges UI

--- src/app/CorePages/badges/badges.component.ts
+++ src/app/CorePages/badges/badges.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../shared/services/data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-badges',
@@ -8,22 +9,63 @@ import { DataService } from '../../shared/services/data.service';
 })
 export class BadgesComponent implements OnInit {
 
-  badgesList: any;
+  badgesList: any = [];
+  badges: any;
+  badges1: any;
 
   constructor(
-    private _dataService: DataService
-  ) { }
+    private _dataService: DataService,
+    private _router: Router,
+    ) { }
 
   ngOnInit() {
     this.getbadgesList();
+
+    this.badges = [{
+      'imageURL': '../../../assets/img/brand/medal.svg',
+      'id': "ACE",
+      'given': 0,
+      'received': 0
+    }, {
+      'imageURL': '../../../assets/img/brand/medal_1.svg',
+      'id': "Maven",
+      'given': 0,
+      'received': 0
+    }, {
+      'imageURL': '../../../assets/img/brand/medal_1.svg',
+      'id': "Customer Delight",
+      'given': 0,
+      'received': 0
+    }];
+
+    this.badges1 = [{
+      'imageURL': '../../../assets/img/brand/medal_3.svg',
+      'id': "Rising Star",
+      'given': 0,
+      'received': 0
+    }, {
+      'imageURL': '../../../assets/img/brand/trophy.svg',
+      'id': "Tech Panel",
+      'given': 0,
+      'received': 0
+    }, {
+      'imageURL': '../../../assets/img/brand/certificate.svg',
+      'id': "Outstanding Performer",
+      'given': 0,
+      'received': 0
+    }];
   }
 
   getbadgesList() {
-    this._dataService.getData('badges')
+    this._dataService.getData('badgedetails')
       .subscribe((res) => {
         console.log(res);
         this.badgesList = res;
       });
+  }
+
+  routeWithData(data) {
+    this._router.navigateByUrl('/badges/add', { state: {badges: data} });
   }
 
 }
