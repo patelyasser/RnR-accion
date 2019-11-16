6c798b68d707a450a7fcc99975173d3d6acf73bd
Dhruv V <dhruv.vaghela@accionlabs.com>
2019-11-16 11:25:26 +0530

    icons added in kudos form

--- src/app/CorePages/kudos/give-kudos/give-kudos.component.ts
+++ src/app/CorePages/kudos/give-kudos/give-kudos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-give-kudos',
@@ -11,9 +12,32 @@ export class GiveKudosComponent implements OnInit {
   formURL: string = environment.formURL;
   kudosFormURL: string = `${this.formURL}/kudos-details`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.setIcons();
+  }
+  
+  setIcons(): void {
+    this.http.get(this.kudosFormURL).subscribe((data: any) => {
+
+      let className = 'form-check-label label-position-right'
+      let form = Array.from(document.getElementsByClassName(className));
+
+      let imageLabels = [['ok', 'Good'],['thumbs-up', 'Very Good'],['clap', 'Excellent']]
+
+      imageLabels.forEach((label, index: number) => {
+        let img = document.createElement('img');
+        
+        img.setAttribute('data-toggle', 'tooltip')
+        img.setAttribute('title', label[1])
+        img.setAttribute('src', `assets/img/brand/${label[0]}.svg`)
+        
+        img.classList.add('form-icons');
+
+        form[index].appendChild(img);
+      });
+    });
   }
 
 }
